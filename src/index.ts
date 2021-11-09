82117bb0e4ff5bf92e884aace774f3692585701c
Ruanqin <teldrassildruid@gmail.com>
2021-11-08 22:34:25 -0500

    create User and Company class

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,8 @@
-console.log("Hi there!!!");
 // run parcel index.html in terminal (at root)
+import { User } from "./User";
+import { Company } from "./Company";
+
+const user = new User();
+console.log(user);
+const company = new Company();
+console.log(company);
